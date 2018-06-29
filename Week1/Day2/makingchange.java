8272c70633699fc2f82db2b6cfe60207e8cd1265
RakeshDasapathri <dasapathrirakesh@gmail.com>
2018-06-29 11:58:36 +0530

    Add files via upload

---
+++ Week1/Day2/makingchange.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class makingchange {
+    public static int  count(int[] denom,int amount)
+    {
+        int[] change=new int[amount+1];
+        change[0]=1;
+        for(int i=0;i<denom.length;i++)
+        {
+            for(int j=denom[i];j<change.length;j++)
+            {
+                change[j]=change[j]+change[j-denom[i]];
+            }
+        }
+        return change[change.length-1];
+    }
+    public static void main(String[] args) {
+        int denom[] = {1,2,3};
+        int amount = 4;
+        System.out.println(count(denom,amount));
+
+    }
+}
