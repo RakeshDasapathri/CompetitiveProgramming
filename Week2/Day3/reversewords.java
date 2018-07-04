03695d9d3ecdd2d34ea4c16c70cae1b824cca12b
RakeshDasapathri <dasapathrirakesh@gmail.com>
2018-07-04 12:30:17 +0530

    Add files via upload

---
+++ Week2/Day3/reversewords.java
@@ -0,0 +1,117 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+    public  void reverse(char[] input,int start,int end)
+    {
+        while(start<end)
+        {
+            char temp=input[start];
+            input[start]=input[end];
+            input[end]=temp;
+            start++;
+            end--;
+        }
+
+    }
+    public String reverseWords(char[] input)
+    {
+        int word_begin=0;
+        int index=1;
+        while(index<input.length)
+        {
+            if(input[index]==' ')
+            {
+                reverse(input,word_begin,index-1);
+                word_begin=index+1;
+            }
+            else if(index==input.length-1)
+                reverse(input,word_begin,index);
+            index++;
+        }
+
+        reverse(input,0,input.length-1);
+
+        return String.valueOf(input);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void oneWordTest() {
+        final char[] expected = "vault".toCharArray();
+        final char[] actual = "vault".toCharArray();
+        reverseWords(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void twoWordsTest() {
+        final char[] expected = "cake thief".toCharArray();
+        final char[] actual = "thief cake".toCharArray();
+        reverseWords(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void threeWordsTest() {
+        final char[] expected = "get another one".toCharArray();
+        final char[] actual = "one another get".toCharArray();
+        reverseWords(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void multipleWordsSameLengthTest() {
+        final char[] expected = "the cat ate the rat".toCharArray();
+        final char[] actual = "rat the ate cat the".toCharArray();
+        reverseWords(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void multipleWordsDifferentLengthsTest() {
+        final char[] expected = "chocolate bundt cake is yummy".toCharArray();
+        final char[] actual = "yummy is cake bundt chocolate".toCharArray();
+        reverseWords(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void emptyStringTest() {
+        final char[] expected = "".toCharArray();
+        final char[] actual = "".toCharArray();
+        reverseWords(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
