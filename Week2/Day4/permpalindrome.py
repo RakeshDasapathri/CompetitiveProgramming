5ee6e9fe406a1aaf5fe4b66489fce0eb9c6b168d
RakeshDasapathri <dasapathrirakesh@gmail.com>
2018-07-05 17:43:19 +0530

    Add files via upload

---
+++ Week2/Day4/permpalindrome.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+def has_palindrome_permutation(the_string):
+
+    
+    res = [0]*26
+    for c in the_string:
+        res[ord(c) - 97] = (res[ord(c) - 97] + 1)%2
+    return (res.count(1) == 1) or (res.count(1) == 0)
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
