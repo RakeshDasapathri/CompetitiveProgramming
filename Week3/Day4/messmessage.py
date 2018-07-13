8284c20af2a8444c66e31ee5d331e210dfc45fb4
RakeshDasapathri <dasapathrirakesh@gmail.com>
2018-07-13 15:48:14 +0530

    Add files via upload

---
+++ Week3/Day4/messmessage.py
@@ -0,0 +1,81 @@
+import unittest
+
+
+def bfs_get_path(graph, start, end, path=[]):
+
+    # Find the shortest route in the network between the two users
+    if (start not in graph) or (end not in graph):
+        raise Exception('')
+    path = path + [start]
+    if start == end:
+        return path
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = bfs_get_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.graph = {
+            'a': ['b', 'c', 'd'],
+            'b': ['a', 'd'],
+            'c': ['a', 'e'],
+            'd': ['a', 'b'],
+            'e': ['c'],
+            'f': ['g'],
+            'g': ['f'],
+        }
+
+    def test_two_hop_path_1(self):
+        actual = bfs_get_path(self.graph, 'a', 'e')
+        expected = ['a', 'c', 'e']
+        self.assertEqual(actual, expected)
+
+    def test_two_hop_path_2(self):
+        actual = bfs_get_path(self.graph, 'd', 'c')
+        expected = ['d', 'a', 'c']
+        self.assertEqual(actual, expected)
+
+    def test_one_hop_path_1(self):
+        actual = bfs_get_path(self.graph, 'a', 'c')
+        expected = ['a', 'c']
+        self.assertEqual(actual, expected)
+
+    def test_one_hop_path_2(self):
+        actual = bfs_get_path(self.graph, 'f', 'g')
+        expected = ['f', 'g']
+        self.assertEqual(actual, expected)
+
+    def test_one_hop_path_3(self):
+        actual = bfs_get_path(self.graph, 'g', 'f')
+        expected = ['g', 'f']
+        self.assertEqual(actual, expected)
+
+    def test_zero_hop_path(self):
+        actual = bfs_get_path(self.graph, 'a', 'a')
+        expected = ['a']
+        self.assertEqual(actual, expected)
+
+    def test_no_path(self):
+        actual = bfs_get_path(self.graph, 'a', 'f')
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_start_node_not_present(self):
+        with self.assertRaises(Exception):
+            bfs_get_path(self.graph, 'h', 'a')
+
+    def test_end_node_not_present(self):
+        with self.assertRaises(Exception):
+            bfs_get_path(self.graph, 'a', 'h')
+
+
+unittest.main(verbosity=2)
